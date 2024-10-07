01e2c7c2308b51b72b5e706cfcaa18178c904e87
Sakhanskyi Oleksandr <L20220126@SSPTAJI.CZ>
2024-10-07 13:08:05 +0200

    update

--- app.js
+++ app.js
@@ -9,3 +9,32 @@ if ('serviceWorker' in navigator) {
             });
     });
 }
+
+function calculate(){
+const hodnota_a=document.getElementById('a').value;
+const hodnota_b=document.getElementById('b').value;
+const hodnota_c=document.getElementById('c').value;
+
+let res1;
+let res2;
+
+let disc=(b*b)-(4*a*c)
+
+if(disc>=0)
+{if(d==0)
+    {
+        res1=(-b)/(2*a)
+        res2=(-b)/(2*a)
+    }
+    res1=(-b + Math.sqrt(disc)/(2*a))
+    res2=(-b - Math.sqrt(disc)/(2*a))
+
+}
+else
+{
+    res2="nelze najit vysledek"
+    res1="nelze najit vysledek"
+}
+
+
+}
